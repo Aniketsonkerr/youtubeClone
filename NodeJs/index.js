a0ca912fd6f2384545d87f83d9737f2142255613
aniketsonkerr <aniketsonkerr@gmail.com>
2025-08-10 17:59:33 +0530

    added dotenv

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,21 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import { userRouter } from "./routes/user.routes.js";
 import { videoRouter } from "./routes/video.routes.js";
 import { channelRouter } from "./routes/channel.routes.js";
 import { commentRouter } from "./routes/comment.routes.js";
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server running on Port 3000");
+dotenv.config({path:"./.env"})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
 });
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-  "mongodb+srv://Aniketsonkerr:Anni123@cluster0.cv621om.mongodb.net/"
+process.env.MONGODB_URI
 );
 
 const db = mongoose.connection;
